5b6e531097d58b2984de2f982f802a29158db7cc
Fernando Nogueira <fernandojnn@gmail.com>
2015-03-15 03:05:37 -0300

    Setting latest JDK supported version to Java 8.

--- src/main/java/japiblueprint/MainProcessor.java
+++ src/main/java/japiblueprint/MainProcessor.java
@@ -34,7 +34,7 @@ import java.util.*;
                 "japiblueprint.annotations.ApiResourceAttr"
         }
 )
-@SupportedSourceVersion(SourceVersion.RELEASE_7)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedOptions("apiOutput")
 public class MainProcessor extends AbstractProcessor {
 
